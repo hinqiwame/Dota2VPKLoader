f9de729cd01f3da97f1748ac2954136e5ea514a5
Roman Frolov <77743895+meth1337@users.noreply.github.com>
2023-05-15 11:32:09 +0300

    Update compiler.bat

--- src/compiler.bat
+++ src/compiler.bat
@@ -1,5 +1,10 @@
 @echo off
-echo [~] Compiling Dota2VPKLoader...
+echo [~] Компиляция Dota2VPKLoader...
 pyinstaller -F --onefile --icon=NONE Dota2VPKLoader.py
-echo [+] Done.
+rd /s /q build
+del /q Dota2VPKLoader.spec
+if not exist Compiled mkdir Compiled
+move /y dist\*.* Compiled
+rd /s /q dist
+echo [+] Готово.
 pause
