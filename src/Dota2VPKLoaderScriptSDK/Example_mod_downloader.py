0c6c30b4aa285c0b5bf61f6013c14df7e703e8d8
Roman Frolov <77743895+meth1337@users.noreply.github.com>
2023-05-16 10:40:29 +0300

    Пример пользовательского скрипта для скачивания мода с сервера

---
+++ src/Dota2VPKLoaderScriptSDK/Example_mod_downloader.py
@@ -0,0 +1,18 @@
+# В этом примере скрипт будет скачивать мод прямо после нахождения папки Dota 2.
+import requests  # Импортируем библиотеку requests для работы с HTTP запросами
+
+def main():  # Определяем главную функцию
+    # Отправляем HTTP запрос для получения файла по указанному URL
+    response = requests.get("https://raw.githubusercontent.com/meth1337/Dota2VPKLoader/main/src/Dota2VPKLoaderScriptSDK/pak01_dir.vpk", stream=True)
+    response.raise_for_status()  # Проверяем статус ответа сервера
+
+    # Открываем файл с указанным именем для записи в двоичном режиме
+    with open("pak01_dir.vpk", "wb") as f:
+        # Читаем содержимое ответа сервера по частям (chunks)
+        for chunk in response.iter_content(chunk_size=8192):
+            # Если chunk не пустой, записываем его в файл
+            if chunk:
+                f.write(chunk)
+    
+    # Выводим сообщение о успешной загрузке файла
+    print("[+] Мод с сервера скачан!")
